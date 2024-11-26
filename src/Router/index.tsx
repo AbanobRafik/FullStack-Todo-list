c21573b3087d2d62c981a51fbfd0e959dd69701d
abanob rafik <bobarafik0123@gmail.com>
2024-11-26 19:33:42 +0200

    Enable React Router v7 future flags for smoother transitions and better routing

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -94,7 +94,17 @@ const router = createBrowserRouter(
       />
       <Route path="*" element={<ErrorPage />} />
     </Route>
-  )
+  ),
+  {
+    future: {
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
+      v7_relativeSplatPath: true,
+      v7_startTransition: true,
+    },
+  }
 );
 
 export default router;
